922d13eda1be6dae5051cd6c3dc07efbd921ee1c
Qihang <qihang.li@outlook.com>
2024-01-04 14:49:37 +0100

    feat: inference

--- engine.py
+++ engine.py
@@ -21,6 +21,13 @@ from datasets.coco_eval import CocoEvaluator
 from datasets.panoptic_eval import PanopticEvaluator
 from datasets.data_prefetcher import data_prefetcher
 
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import torchvision.transforms as T
+import torchvision.transforms.functional as F
+from pycocotools.coco import COCO
+
+
 
 def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
                     data_loader: Iterable, optimizer: torch.optim.Optimizer,
@@ -164,3 +171,123 @@ def evaluate(model, criterion, postprocessors, data_loader, base_ds, device, out
         stats['PQ_th'] = panoptic_res["Things"]
         stats['PQ_st'] = panoptic_res["Stuff"]
     return stats, coco_evaluator
+
+def testcarla(model,postprocessors, data_loader,device):
+    dataiter = iter(data_loader)
+    for index, value in enumerate(dataiter):
+        samples, targets = value
+        samples_gpu =samples.to(device)
+        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+        outputs = model(samples_gpu)
+        orig_target_sizes = torch.stack([t["orig_size"] for t in targets], dim=0)
+        orig_target_sizes =orig_target_sizes.to(device)
+        results = postprocessors['bbox'](outputs, orig_target_sizes)
+        thershold = 0.4
+        scores = results[0]['scores']
+        labels = results[0]['labels']
+        boxes = results[0]['boxes']
+        select_mask = scores > thershold
+        os.makedirs('output_images', exist_ok=True)
+        pred_dict = {'boxes': boxes[select_mask]}
+
+        inv_normalize = T.Normalize(
+        mean=[-0.485 / 0.229, -0.456 / 0.224, -0.406 / 0.225],
+        std=[1 / 0.229, 1 / 0.224, 1 / 0.225])
+        original_image = inv_normalize(samples.tensors[0])
+        image = F.to_pil_image(original_image)
+
+        fig, ax = plt.subplots(1)
+        ax.imshow(image)
+        for box, label in zip(pred_dict['boxes'].cpu(),labels.cpu()):
+            x0, y0, x1, y1 = box[0], box[1], box[2], box[3]
+            cat = dictionary(label.item())
+            rect = patches.Rectangle((x0,y0), x1-x0, y1-y0, linewidth=2, edgecolor='r', facecolor='none')
+            ax.add_patch(rect)
+            ax.text(x0, y0, f"{cat}", color = 'r')
+        plt.savefig(f'output_images/image_{index}.png')        
+        plt.close()
+
+def dictionary(number):
+    label_map = {
+    1: 'person',
+    2: 'bicycle',
+    3: 'car',
+    4: 'motorbike',
+    5: 'aeroplane',
+    6: 'bus',
+    7: 'train',
+    8: 'truck',
+    9: 'boat',
+    10: 'traffic light',
+    11: 'fire hydrant',
+    13: 'stop sign',
+    14: 'parking meter',
+    15: 'bench',
+    16: 'bird',
+    17: 'cat',
+    18: 'dog',
+    19: 'horse',
+    20: 'sheep',
+    21: 'cow',
+    22: 'elephant',
+    23: 'bear',
+    24: 'zebra',
+    25: 'giraffe',
+    27: 'backpack',
+    28: 'umbrella',
+    31: 'handbag',
+    32: 'tie',
+    33: 'suitcase',
+    34: 'frisbee',
+    35: 'skis',
+    36: 'snowboard',
+    37: 'sports ball',
+    38: 'kite',
+    39: 'baseball bat',
+    40: 'baseball glove',
+    41: 'skateboard',
+    42: 'surfboard',
+    43: 'tennis racket',
+    44: 'bottle',
+    46: 'wine glass',
+    47: 'cup',
+    48: 'fork',
+    49: 'knife',
+    50: 'spoon',
+    51: 'bowl',
+    52: 'banana',
+    53: 'apple',
+    54: 'sandwich',
+    55: 'orange',
+    56: 'broccoli',
+    57: 'carrot',
+    58: 'hot dog',
+    59: 'pizza',
+    60: 'donut',
+    61: 'cake',
+    62: 'chair',
+    63: 'sofa',
+    64: 'pottedplant',
+    65: 'bed',
+    67: 'diningtable',
+    70: 'toilet',
+    72: 'tvmonitor',
+    73: 'laptop',
+    74: 'mouse',
+    75: 'remote',
+    76: 'keyboard',
+    77: 'cell phone',
+    78: 'microwave',
+    79: 'oven',
+    80: 'toaster',
+    81: 'sink',
+    82: 'refrigerator',
+    84: 'book',
+    85: 'clock',
+    86: 'vase',
+    87: 'scissors',
+    88: 'teddy bear',
+    89: 'hair drier',
+    90: 'toothbrush'
+    }
+    return label_map.get(number,"unkonown")
