618fa58833e916aa19156be4a0559362b322b978
Qihang <qihang.li@outlook.com>
2024-01-16 14:32:11 +0100

    feat: black formatting

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -13,6 +13,7 @@ from .torchvision_datasets import CocoDetection
 from .coco import build as build_coco
 from .carla import build_carla
 
+
 def get_coco_api_from_dataset(dataset):
     for _ in range(10):
         # if isinstance(dataset, torchvision.datasets.CocoDetection):
@@ -24,13 +25,16 @@ def get_coco_api_from_dataset(dataset):
 
 
 def build_dataset(image_set, args):
-    if args.dataset_file == 'coco':
+    if args.dataset_file == "coco":
         return build_coco(image_set, args)
-    if args.dataset_file == 'coco_panoptic':
+    if args.dataset_file == "coco_panoptic":
         # to avoid making panopticapi required for coco
         from .coco_panoptic import build as build_coco_panoptic
+
         return build_coco_panoptic(image_set, args)
+
+
 def build_carla_dataset(image_set, args):
-    if args.test_dataset_file == 'carla':
-        return build_carla(image_set, args) 
-    raise ValueError(f'dataset {args.test_dataset_file} not supported')
+    if args.test_dataset_file == "carla":
+        return build_carla(image_set, args)
+    raise ValueError(f"dataset {args.test_dataset_file} not supported")
--- engine.py
+++ engine.py
@@ -28,17 +28,26 @@ import torchvision.transforms.functional as F
 from pycocotools.coco import COCO
 
 
-
-def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
-                    data_loader: Iterable, optimizer: torch.optim.Optimizer,
-                    device: torch.device, epoch: int, max_norm: float = 0):
+def train_one_epoch(
+    model: torch.nn.Module,
+    criterion: torch.nn.Module,
+    data_loader: Iterable,
+    optimizer: torch.optim.Optimizer,
+    device: torch.device,
+    epoch: int,
+    max_norm: float = 0,
+):
     model.train()
     criterion.train()
     metric_logger = utils.MetricLogger(delimiter="  ")
-    metric_logger.add_meter('lr', utils.SmoothedValue(window_size=1, fmt='{value:.6f}'))
-    metric_logger.add_meter('class_error', utils.SmoothedValue(window_size=1, fmt='{value:.2f}'))
-    metric_logger.add_meter('grad_norm', utils.SmoothedValue(window_size=1, fmt='{value:.2f}'))
-    header = 'Epoch: [{}]'.format(epoch)
+    metric_logger.add_meter("lr", utils.SmoothedValue(window_size=1, fmt="{value:.6f}"))
+    metric_logger.add_meter(
+        "class_error", utils.SmoothedValue(window_size=1, fmt="{value:.2f}")
+    )
+    metric_logger.add_meter(
+        "grad_norm", utils.SmoothedValue(window_size=1, fmt="{value:.2f}")
+    )
+    header = "Epoch: [{}]".format(epoch)
     print_freq = 10
 
     prefetcher = data_prefetcher(data_loader, device, prefetch=True)
@@ -49,14 +58,20 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
         outputs = model(samples)
         loss_dict = criterion(outputs, targets)
         weight_dict = criterion.weight_dict
-        losses = sum(loss_dict[k] * weight_dict[k] for k in loss_dict.keys() if k in weight_dict)
+        losses = sum(
+            loss_dict[k] * weight_dict[k] for k in loss_dict.keys() if k in weight_dict
+        )
 
         # reduce losses over all GPUs for logging purposes
         loss_dict_reduced = utils.reduce_dict(loss_dict)
-        loss_dict_reduced_unscaled = {f'{k}_unscaled': v
-                                      for k, v in loss_dict_reduced.items()}
-        loss_dict_reduced_scaled = {k: v * weight_dict[k]
-                                    for k, v in loss_dict_reduced.items() if k in weight_dict}
+        loss_dict_reduced_unscaled = {
+            f"{k}_unscaled": v for k, v in loss_dict_reduced.items()
+        }
+        loss_dict_reduced_scaled = {
+            k: v * weight_dict[k]
+            for k, v in loss_dict_reduced.items()
+            if k in weight_dict
+        }
         losses_reduced_scaled = sum(loss_dict_reduced_scaled.values())
 
         loss_value = losses_reduced_scaled.item()
@@ -69,13 +84,17 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
         optimizer.zero_grad()
         losses.backward()
         if max_norm > 0:
-            grad_total_norm = torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm)
+            grad_total_norm = torch.nn.utils.clip_grad_norm_(
+                model.parameters(), max_norm
+            )
         else:
             grad_total_norm = utils.get_total_grad_norm(model.parameters(), max_norm)
         optimizer.step()
 
-        metric_logger.update(loss=loss_value, **loss_dict_reduced_scaled, **loss_dict_reduced_unscaled)
-        metric_logger.update(class_error=loss_dict_reduced['class_error'])
+        metric_logger.update(
+            loss=loss_value, **loss_dict_reduced_scaled, **loss_dict_reduced_unscaled
+        )
+        metric_logger.update(class_error=loss_dict_reduced["class_error"])
         metric_logger.update(lr=optimizer.param_groups[0]["lr"])
         metric_logger.update(grad_norm=grad_total_norm)
 
@@ -87,20 +106,24 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
 
 
 @torch.no_grad()
-def evaluate(model, criterion, postprocessors, data_loader, base_ds, device, output_dir):
+def evaluate(
+    model, criterion, postprocessors, data_loader, base_ds, device, output_dir
+):
     model.eval()
     criterion.eval()
 
     metric_logger = utils.MetricLogger(delimiter="  ")
-    metric_logger.add_meter('class_error', utils.SmoothedValue(window_size=1, fmt='{value:.2f}'))
-    header = 'Test:'
+    metric_logger.add_meter(
+        "class_error", utils.SmoothedValue(window_size=1, fmt="{value:.2f}")
+    )
+    header = "Test:"
 
-    iou_types = tuple(k for k in ('segm', 'bbox') if k in postprocessors.keys())
+    iou_types = tuple(k for k in ("segm", "bbox") if k in postprocessors.keys())
     coco_evaluator = CocoEvaluator(base_ds, iou_types)
     # coco_evaluator.coco_eval[iou_types[0]].params.iouThrs = [0, 0.1, 0.5, 0.75]
 
     panoptic_evaluator = None
-    if 'panoptic' in postprocessors.keys():
+    if "panoptic" in postprocessors.keys():
         panoptic_evaluator = PanopticEvaluator(
             data_loader.dataset.ann_file,
             data_loader.dataset.ann_folder,
@@ -117,26 +140,39 @@ def evaluate(model, criterion, postprocessors, data_loader, base_ds, device, out
 
         # reduce losses over all GPUs for logging purposes
         loss_dict_reduced = utils.reduce_dict(loss_dict)
-        loss_dict_reduced_scaled = {k: v * weight_dict[k]
-                                    for k, v in loss_dict_reduced.items() if k in weight_dict}
-        loss_dict_reduced_unscaled = {f'{k}_unscaled': v
-                                      for k, v in loss_dict_reduced.items()}
-        metric_logger.update(loss=sum(loss_dict_reduced_scaled.values()),
-                             **loss_dict_reduced_scaled,
-                             **loss_dict_reduced_unscaled)
-        metric_logger.update(class_error=loss_dict_reduced['class_error'])
+        loss_dict_reduced_scaled = {
+            k: v * weight_dict[k]
+            for k, v in loss_dict_reduced.items()
+            if k in weight_dict
+        }
+        loss_dict_reduced_unscaled = {
+            f"{k}_unscaled": v for k, v in loss_dict_reduced.items()
+        }
+        metric_logger.update(
+            loss=sum(loss_dict_reduced_scaled.values()),
+            **loss_dict_reduced_scaled,
+            **loss_dict_reduced_unscaled,
+        )
+        metric_logger.update(class_error=loss_dict_reduced["class_error"])
 
         orig_target_sizes = torch.stack([t["orig_size"] for t in targets], dim=0)
-        results = postprocessors['bbox'](outputs, orig_target_sizes)
-        if 'segm' in postprocessors.keys():
+        results = postprocessors["bbox"](outputs, orig_target_sizes)
+        if "segm" in postprocessors.keys():
             target_sizes = torch.stack([t["size"] for t in targets], dim=0)
-            results = postprocessors['segm'](results, outputs, orig_target_sizes, target_sizes)
-        res = {target['image_id'].item(): output for target, output in zip(targets, results)}
+            results = postprocessors["segm"](
+                results, outputs, orig_target_sizes, target_sizes
+            )
+        res = {
+            target["image_id"].item(): output
+            for target, output in zip(targets, results)
+        }
         if coco_evaluator is not None:
             coco_evaluator.update(res)
 
         if panoptic_evaluator is not None:
-            res_pano = postprocessors["panoptic"](outputs, target_sizes, orig_target_sizes)
+            res_pano = postprocessors["panoptic"](
+                outputs, target_sizes, orig_target_sizes
+            )
             for i, target in enumerate(targets):
                 image_id = target["image_id"].item()
                 file_name = f"{image_id:012d}.png"
@@ -162,132 +198,137 @@ def evaluate(model, criterion, postprocessors, data_loader, base_ds, device, out
         panoptic_res = panoptic_evaluator.summarize()
     stats = {k: meter.global_avg for k, meter in metric_logger.meters.items()}
     if coco_evaluator is not None:
-        if 'bbox' in postprocessors.keys():
-            stats['coco_eval_bbox'] = coco_evaluator.coco_eval['bbox'].stats.tolist()
-        if 'segm' in postprocessors.keys():
-            stats['coco_eval_masks'] = coco_evaluator.coco_eval['segm'].stats.tolist()
+        if "bbox" in postprocessors.keys():
+            stats["coco_eval_bbox"] = coco_evaluator.coco_eval["bbox"].stats.tolist()
+        if "segm" in postprocessors.keys():
+            stats["coco_eval_masks"] = coco_evaluator.coco_eval["segm"].stats.tolist()
     if panoptic_res is not None:
-        stats['PQ_all'] = panoptic_res["All"]
-        stats['PQ_th'] = panoptic_res["Things"]
-        stats['PQ_st'] = panoptic_res["Stuff"]
+        stats["PQ_all"] = panoptic_res["All"]
+        stats["PQ_th"] = panoptic_res["Things"]
+        stats["PQ_st"] = panoptic_res["Stuff"]
     return stats, coco_evaluator
 
-def testcarla(model,postprocessors, data_loader,device):
+
+def testcarla(model, postprocessors, data_loader, device):
     dataiter = iter(data_loader)
     for index, value in enumerate(dataiter):
         samples, targets = value
-        samples_gpu =samples.to(device)
+        samples_gpu = samples.to(device)
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
         outputs = model(samples_gpu)
         orig_target_sizes = torch.stack([t["orig_size"] for t in targets], dim=0)
-        orig_target_sizes =orig_target_sizes.to(device)
-        results = postprocessors['bbox'](outputs, orig_target_sizes)
+        orig_target_sizes = orig_target_sizes.to(device)
+        results = postprocessors["bbox"](outputs, orig_target_sizes)
         thershold = 0.4
-        scores = results[0]['scores']
-        labels = results[0]['labels']
-        boxes = results[0]['boxes']
+        scores = results[0]["scores"]
+        labels = results[0]["labels"]
+        boxes = results[0]["boxes"]
         select_mask = scores > thershold
-        os.makedirs('output_images', exist_ok=True)
-        pred_dict = {'boxes': boxes[select_mask]}
+        os.makedirs("output_images", exist_ok=True)
+        pred_dict = {"boxes": boxes[select_mask]}
 
         inv_normalize = T.Normalize(
-        mean=[-0.485 / 0.229, -0.456 / 0.224, -0.406 / 0.225],
-        std=[1 / 0.229, 1 / 0.224, 1 / 0.225])
+            mean=[-0.485 / 0.229, -0.456 / 0.224, -0.406 / 0.225],
+            std=[1 / 0.229, 1 / 0.224, 1 / 0.225],
+        )
         original_image = inv_normalize(samples.tensors[0])
         image = F.to_pil_image(original_image)
 
         fig, ax = plt.subplots(1)
         ax.imshow(image)
-        for box, label in zip(pred_dict['boxes'].cpu(),labels.cpu()):
+        for box, label in zip(pred_dict["boxes"].cpu(), labels.cpu()):
             x0, y0, x1, y1 = box[0], box[1], box[2], box[3]
             cat = dictionary(label.item())
-            rect = patches.Rectangle((x0,y0), x1-x0, y1-y0, linewidth=2, edgecolor='r', facecolor='none')
+            rect = patches.Rectangle(
+                (x0, y0), x1 - x0, y1 - y0, linewidth=2, edgecolor="r", facecolor="none"
+            )
             ax.add_patch(rect)
-            ax.text(x0, y0, f"{cat}", color = 'r')
-        plt.savefig(f'output_images/image_{index}.png')        
+            ax.text(x0, y0, f"{cat}", color="r")
+        plt.savefig(f"output_images/image_{index}.png")
         plt.close()
+
 
 def dictionary(number):
     label_map = {
-    1: 'person',
-    2: 'bicycle',
-    3: 'car',
-    4: 'motorbike',
-    5: 'aeroplane',
-    6: 'bus',
-    7: 'train',
-    8: 'truck',
-    9: 'boat',
-    10: 'traffic light',
-    11: 'fire hydrant',
-    13: 'stop sign',
-    14: 'parking meter',
-    15: 'bench',
-    16: 'bird',
-    17: 'cat',
-    18: 'dog',
-    19: 'horse',
-    20: 'sheep',
-    21: 'cow',
-    22: 'elephant',
-    23: 'bear',
-    24: 'zebra',
-    25: 'giraffe',
-    27: 'backpack',
-    28: 'umbrella',
-    31: 'handbag',
-    32: 'tie',
-    33: 'suitcase',
-    34: 'frisbee',
-    35: 'skis',
-    36: 'snowboard',
-    37: 'sports ball',
-    38: 'kite',
-    39: 'baseball bat',
-    40: 'baseball glove',
-    41: 'skateboard',
-    42: 'surfboard',
-    43: 'tennis racket',
-    44: 'bottle',
-    46: 'wine glass',
-    47: 'cup',
-    48: 'fork',
-    49: 'knife',
-    50: 'spoon',
-    51: 'bowl',
-    52: 'banana',
-    53: 'apple',
-    54: 'sandwich',
-    55: 'orange',
-    56: 'broccoli',
-    57: 'carrot',
-    58: 'hot dog',
-    59: 'pizza',
-    60: 'donut',
-    61: 'cake',
-    62: 'chair',
-    63: 'sofa',
-    64: 'pottedplant',
-    65: 'bed',
-    67: 'diningtable',
-    70: 'toilet',
-    72: 'tvmonitor',
-    73: 'laptop',
-    74: 'mouse',
-    75: 'remote',
-    76: 'keyboard',
-    77: 'cell phone',
-    78: 'microwave',
-    79: 'oven',
-    80: 'toaster',
-    81: 'sink',
-    82: 'refrigerator',
-    84: 'book',
-    85: 'clock',
-    86: 'vase',
-    87: 'scissors',
-    88: 'teddy bear',
-    89: 'hair drier',
-    90: 'toothbrush'
+        1: "person",
+        2: "bicycle",
+        3: "car",
+        4: "motorbike",
+        5: "aeroplane",
+        6: "bus",
+        7: "train",
+        8: "truck",
+        9: "boat",
+        10: "traffic light",
+        11: "fire hydrant",
+        13: "stop sign",
+        14: "parking meter",
+        15: "bench",
+        16: "bird",
+        17: "cat",
+        18: "dog",
+        19: "horse",
+        20: "sheep",
+        21: "cow",
+        22: "elephant",
+        23: "bear",
+        24: "zebra",
+        25: "giraffe",
+        27: "backpack",
+        28: "umbrella",
+        31: "handbag",
+        32: "tie",
+        33: "suitcase",
+        34: "frisbee",
+        35: "skis",
+        36: "snowboard",
+        37: "sports ball",
+        38: "kite",
+        39: "baseball bat",
+        40: "baseball glove",
+        41: "skateboard",
+        42: "surfboard",
+        43: "tennis racket",
+        44: "bottle",
+        46: "wine glass",
+        47: "cup",
+        48: "fork",
+        49: "knife",
+        50: "spoon",
+        51: "bowl",
+        52: "banana",
+        53: "apple",
+        54: "sandwich",
+        55: "orange",
+        56: "broccoli",
+        57: "carrot",
+        58: "hot dog",
+        59: "pizza",
+        60: "donut",
+        61: "cake",
+        62: "chair",
+        63: "sofa",
+        64: "pottedplant",
+        65: "bed",
+        67: "diningtable",
+        70: "toilet",
+        72: "tvmonitor",
+        73: "laptop",
+        74: "mouse",
+        75: "remote",
+        76: "keyboard",
+        77: "cell phone",
+        78: "microwave",
+        79: "oven",
+        80: "toaster",
+        81: "sink",
+        82: "refrigerator",
+        84: "book",
+        85: "clock",
+        86: "vase",
+        87: "scissors",
+        88: "teddy bear",
+        89: "hair drier",
+        90: "toothbrush",
     }
-    return label_map.get(number,"unkonown")
+    return label_map.get(number, "unkonown")
